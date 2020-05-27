4dbec836d043f4676264febdd7920c07bd33c02f
mario <MSenden>
2020-05-27 10:00:15 +0200

    added Gaussian function | added nline_processor class

--- code/python/cni_toolbox/gadgets.py
+++ code/python/cni_toolbox/gadgets.py
@@ -28,8 +28,31 @@ without being clearly assignable to one specific tool.
 
 import numpy as np
 import scipy as sc
+from scipy.fft import fft, ifft
 from scipy.special import gamma
 
+
+def gaussian(mu_x, mu_y, sigma, x, y):
+    '''
+    Parameters
+    ----------
+    mu_x: float
+        center of Gaussian along x direction
+    mu_y: float
+        center of Gaussian along y direction
+    sigma: float
+        spread of Gaussian
+    x: floating point array (1D)
+        x-coordinates
+    y: floating point array (1D)
+        y-coordinates
+
+    Returns
+    -------
+    floating point array
+    '''
+
+    return np.exp(-((x - mu_x)**2 + (y - mu_y)**2) / (2 * sigma**2))
 
 def two_gamma(timepoints):
     '''
@@ -125,3 +148,108 @@ def correct_autocorr(X, W):
         X_corrected[:, j] = np.matmul(x_sliced, W)
 
     return X_corrected
+
+ class online_processor:
+     '''
+    Class for real time processing of fMRI data and stimulus respone.
+
+    obj = online_processor(n_channels) creates an instance of the class.
+    n_channels determines the number of independent channels (voxels,
+    pixels etc.) to which processing is applied
+
+    optional inputs are
+      - sampling_rate : sampling rate of fMRI acquisition (TR) in seconds (default =  2)
+      - l_kernel      : length of hemodynic convolution kernel in seconds (default = 34)
+
+    this class has the following functions
+
+      - x_conv = online_processor.convolve(self, x)
+        performs one step of real-time convolution
+      - x_next = online_processor.update(self,x)
+        performs one step of real-time z-transformation
+      - online_processor.reset()
+        resets all variables tracking the signal
+    '''
+
+    def __init__(n_channels, sampling_rate = 2., l_kernel = 34):
+
+        self.n_channels = n_channels
+        self.p_sampling = sampling_rate
+        self.l_kernel = l_kernel
+
+        self.step = 1
+        self.mean = np.zeros(self.n_channels)
+        self.previous_mean = np.zeros(self.n_channels)
+        self.M2 = np.ones(self.n_channels)
+        self.sigma = np.zeros(self.n_channels)
+
+        self.l_subsampled = int(self.l_kernel / self.p_sampling) - 1
+        timepoints = np.arange(0., self.l_kernel, self.p_sampling)
+        self.hrf_fft = fft(two_gamma(timepoints), axis=0)
+        self.x_conv = np.zeros((self.l_subsampled + 1, self.n_channels))
+
+    def convolve(self, x):
+        '''
+        Parameters
+        ----------
+        x: floating point array
+            current data sample
+
+        Returns
+        -------
+        floating point array
+            convolved sample
+        '''
+
+        x_fft = fft(np.vstack((x, np.zeros((self.l_subsampled, self.n_channels)))))
+        self.conv_x = np.vstack((self.conv_x, np.zeros(self.n_channels)))
+        self.conv_x[self.step:self.step + self.l_subsampled, :] += ifft(
+            x_fft * self.kernel_fft)
+
+        return self.conv_x[self.step, :]
+
+    def update(self, x):
+        '''
+        Parameters
+        ----------
+        x: floating point array
+            current data sample
+
+        Returns
+        -------
+        floating point array
+            z-normalized sample
+        '''
+
+        self.__update_mean__(x);
+        self.__update_sigma__(x);
+        self.step += 1
+
+        return (x - self.mean) ./ self.sigma
+
+    def reset(self):
+        '''
+        resets all variables tracking the signal
+        '''
+
+        self.step = 1
+        self.mean = np.zeros(self.n_channels)
+        self.previous_mean = np.zeros(self.n_channels)
+        self.M2 = np.ones(self.n_channels)
+        self.sigma = np.zeros(self.n_channels)
+
+        self.l_subsampled = int(self.l_kernel / self.p_sampling) - 1
+        timepoints = np.arange(0., self.l_kernel, self.p_sampling)
+        self.hrf_fft = fft(two_gamma(timepoints), axis=0)
+        self.x_conv = np.zeros((self.l_subsampled + 1, self.n_channels))
+
+    def __update_mean__(self, x):
+        self.previous_mean = self.mean
+        self.mean += (x - self.mean) / self.step
+
+    def __update_sigma__(self, x):
+        self.M2 += (x - self.previous_mean) * (x - self.mean)
+        if self.step ==1:
+            self.sigma = np.sqrt(self.M2 / self.step)
+        else:
+            self.sigma = np.sqrt(self.M2 / (self.step - 1))
