bf47462c72b56a51349bbecb7378af858a74b720
mario <MSenden>
2020-05-27 11:34:33 +0200

    reintroduce modulus

--- code/python/cni_toolbox/HGR.py
+++ code/python/cni_toolbox/HGR.py
@@ -220,14 +220,14 @@ class HGR:
             m_image = np.mean(im, axis = 0).transpose()
 
             cx = np.floor(pos / self.r_stimulus)
-            cy = pos self.r_stimulus
+            cy = pos % self.r_stimulus
             results.mu_x[batch] = cx / self.r_stimulus * max_radius * 2 - max_radius
             results.mu_y[batch] = -cy / self.r_stimulus * max_radius * 2 - max_radius
             R = np.sqrt(results.mu_x[batch]**2 + results.mu_y[batch]**2)
             P = np.hstack((m_image, R))
             results.sigma[batch] = np.matmul(P, beta)
 
-            i = int(v / n_mask* 21)
+            i = int(v / n_mask * 21)
             sys.stdout.write('\r')
             sys.stdout.write("[%-20s] %d%%"
                              ('=' * i, 5 * i))
