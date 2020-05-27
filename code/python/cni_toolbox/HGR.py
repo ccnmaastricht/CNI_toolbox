5fe019dd51bbb504afd449a46120640edc5b95d8
mario <MSenden>
2020-05-27 11:37:05 +0200

    corrected unclosed bracket

--- code/python/cni_toolbox/HGR.py
+++ code/python/cni_toolbox/HGR.py
@@ -408,6 +408,6 @@ class HGR:
         n_samples = x.shape[0]
         kernel = np.vstack((self.hrf, np.zeros(n_samples)))
         x = np.vstack((x, np.zeros((np.ceil(self.l_h / self.p_sampling),
-                                    self.n_features)))
+                                    self.n_features))))
         x_conv = ifft(fft(x) * fft(kernel))
         return x_conv[:n_samples]
