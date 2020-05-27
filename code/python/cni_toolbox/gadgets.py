5e7554d5fa863ca275a3d3c21ffc930833102c72
mario <MSenden>
2020-05-27 14:38:19 +0200

    renamed conv_x->conv_x in online processor

--- code/python/cni_toolbox/gadgets.py
+++ code/python/cni_toolbox/gadgets.py
@@ -200,11 +200,11 @@ class online_processor:
         '''
 
         x_fft = fft(np.vstack((x.reshape(1,-1), np.zeros((self.l_subsampled, self.n_channels)))), axis=0)
-        self.conv_x = np.append(self.conv_x, np.zeros(self.n_channels))
-        self.conv_x[self.step:self.step + self.l_subsampled, :] += np.abs(ifft(
+        self.x_conv = np.append(self.x_conv, np.zeros(self.n_channels))
+        self.x_conv[self.step:self.step + self.l_subsampled, :] += np.abs(ifft(
             x_fft * np.expand_dims(self.kernel_fft , axis=1), axis=0))
 
-        return self.conv_x[self.step, :]
+        return self.x_conv[self.step, :]
 
     def update(self, x):
         '''
