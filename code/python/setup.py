5dc0463ecd9f5e6104e3043e76627663fb3b41b9
mario <MSenden>
2020-10-27 14:37:25 +0100

    corrected import from gadgets

--- code/python/setup.py
+++ code/python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cni-tlbx", # Replace with your own username
-    version="1.0.0",
+    version="1.0.1",
     author="Mario Senden",
     author_email="mario.senden@maastrichtuniversity.nl",
     description="computational neuroimaging toolbox",
