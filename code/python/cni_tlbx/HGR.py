corrected sign error in estimation of y coordinates

code/python/cni_tlbx/HGR.py
@@ -263,7 +263,7 @@ class HGR:
         cx = np.floor(pos / self.r_stimulus)
         cy = pos % self.r_stimulus
         results['mu_x'][batch] = cx / self.r_stimulus * max_radius * 2 - max_radius
-        results['mu_y'][batch] = -cy / self.r_stimulus * max_radius * 2 - max_radius
+        results['mu_y'][batch] = -(cy / self.r_stimulus * max_radius * 2 - max_radius)
         R = np.sqrt(results['mu_x'][batch]**2 + results['mu_y'][batch]**2)
         P = np.hstack((m_image.reshape(-1,1), R.reshape(-1,1)))
         results['sigma'][batch] = np.matmul(P, beta)