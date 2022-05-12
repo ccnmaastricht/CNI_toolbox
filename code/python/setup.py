227d2cafd8d9129b7e487cb3eb35c085a78ae147
MSenden <mario.senden@googlemail.com>
2022-05-12 14:51:09 +0200

    updated version number

--- code/python/setup.py
+++ code/python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cni-tlbx", # Replace with your own username
-    version="2.2.2",
+    version="2.2.3",
     author="Mario Senden",
     author_email="mario.senden@maastrichtuniversity.nl",
     description="computational neuroimaging toolbox",
